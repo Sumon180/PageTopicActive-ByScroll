87dd4664ed5215691d36239557e9cdf28a429110
Sumon Hossain <sumonengr.96@gmail.com>
2024-03-23 12:43:03 +0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,119 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const sections = [
+    { id: "section1", label: "Section 1" },
+    { id: "section2", label: "Section 2" },
+    { id: "section3", label: "Section 3" },
+    { id: "section4", label: "Section 4" },
+  ];
+
+  const [currentSection, setCurrentSection] = useState<string>("section1");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll("section");
+      let current: string = "";
+
+      sections.forEach((section: HTMLElement) => {
+        const sectionTop: number = section.offsetTop;
+        const sectionHeight: number = section.clientHeight;
+
+        if (window.scrollY >= sectionTop - sectionHeight / 3) {
+          current = section.id;
+        }
+      });
+
+      setCurrentSection(current);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="flex flex-row-reverse justify-center gap-10">
+        <div>
+          <header className="bg-gray-800 py-4 sticky top-20">
+            <nav className="flex justify-center">
+              <ul className="flex flex-col gap-5">
+                {sections.map((section) => (
+                  <li key={section.id}>
+                    <a
+                      onClick={() => scrollToSection(section.id)}
+                      className={`text-white px-4 py-2 ${
+                        currentSection === section.id ? "bg-gray-900" : ""
+                      }`}
+                    >
+                      {section.label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </header>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="w-1/2">
+          {sections.map((section) => (
+            <section
+              key={section.id}
+              id={section.id}
+              className="portfolio-item h-screen flex flex-col justify-center items-center"
+            >
+              <h2 className="text-4xl font-semibold">{section.label}</h2>
+              <div className="subsection">
+                <h3>Subsection 1</h3>
+                <p>This is the content of subsection 1.</p>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Quam, ad? Libero distinctio corporis eum magni aut modi saepe
+                  repellendus, esse labore velit officia pariatur illum earum
+                  sunt hic accusamus laudantium nisi, fugiat assumenda
+                  reprehenderit nostrum aliquid. Quos voluptates earum error
+                  libero, voluptatum totam maxime suscipit dolores itaque a,
+                  quae hic at quaerat illum et esse incidunt, autem qui.
+                  Similique recusandae voluptates ratione itaque omnis provident
+                  pariatur sed, excepturi magnam perferendis, modi sit
+                  praesentium, neque maiores veniam. Quasi exercitationem
+                  ducimus aliquid odit laboriosam totam earum natus facere sequi
+                  hic? Ullam non rerum a assumenda, quam, dolor ut officia
+                  cumque nesciunt soluta nulla architecto fugit velit nihil
+                  aliquid quis harum laboriosam repudiandae accusantium odit
+                  dolorem cupiditate culpa voluptas. Totam facilis fugiat
+                  numquam labore ullam. Optio quisquam recusandae illum totam
+                  eaque culpa nulla, voluptates suscipit sit doloribus aliquid
+                  corrupti magnam minima aspernatur error sunt cumque fugiat
+                  dolorem porro esse similique quia, ratione soluta unde.
+                  Deserunt nesciunt voluptatem architecto animi repudiandae
+                  praesentium iure vitae magni consequatur tenetur omnis,
+                  aliquid ut ratione ipsa tempora totam a voluptas eius dicta
+                  fugit possimus aspernatur? Consequuntur molestias, sunt,
+                  itaque necessitatibus dignissimos, atque expedita esse illum
+                  ea eligendi possimus in ipsum at quis. Consectetur obcaecati
+                  possimus eos quibusdam fuga!
+                </p>
+              </div>
+              <div className="subsection">
+                <h3>Subsection 2</h3>
+                <p>This is the content of subsection 2.</p>
+              </div>
+            </section>
+          ))}
+        </div>
       </div>
     </main>
   );
